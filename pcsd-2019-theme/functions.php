c9c30add84d0912c2b0d52e2db934aa888e3035d
Espi <joshespi@gmail.com>
2022-06-29 07:10:35 -0600

    update styles version numbers

--- pcsd-2019-theme/functions.php
+++ pcsd-2019-theme/functions.php
@@ -7,9 +7,8 @@ function theme_specific_stylesheet() {
 	wp_register_script( '404easterEgg', get_template_directory_uri() .'/assets/js/404.js', '', '', true );
 
 	/*   CALL ALL CSS AND SCRIPTS FOR SITE */
-	wp_enqueue_style( 'parent-style', get_template_directory_uri() .'/style.css', '', '1.0.07', false);
-	wp_enqueue_script('slick-script', get_template_directory_uri() .'/assets/slick/slick.min.js', array('jquery'), null, true);
-	wp_enqueue_style( 'parent-style', get_template_directory_uri() .'/style.css', '', '1.0.01', false);
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() .'/style.css', '', '1.0.02', false);
+  wp_enqueue_script('slick-script', get_template_directory_uri() .'/assets/slick/slick.min.js', array('jquery'), null, true);
 	wp_enqueue_script('my-custom-scripts', get_template_directory_uri() .'/assets/js/scripts.js', array('jquery', 'slick-script',), '1.0.02', true);
 	if ( is_404() ) {
 		wp_enqueue_script( '404easterEgg', null, null,true);
