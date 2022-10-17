f1c768cc088c708966ea071755ed619d0bc455eb
Espi <joshespi@gmail.com>
2022-10-17 15:29:10 -0600

    Sunset Child theme consolidated

---
+++ sunset-child/template-parts/mega-menu-dropdowns-titlei.php
@@ -0,0 +1,75 @@
+				<div class="dropDownColumn">
+				<?php
+					//checks if this template section is loading into the menu or into the sidebar/content area and assigns the proper heading
+					if(is_page( 'title-i' ) || in_array( 623, get_post_ancestors($post))){
+						?>
+						<h2><img src="https://sunset.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Title I Information</h2>
+						<?php
+					} else {
+						?>
+						<h3><img src="https://sunset.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Title I Information</h3>
+						<?php
+					}
+				?>
+						<ul>
+							<li class="int"><a href="https://sunset.provo.edu/title-i/what-is-title-i/">What is Title I?</a></li>
+							<li class="int"><a href="https://sunset.provo.edu/title-i/patrons-of-title-1-schools-have-the-right-to-know/">Patrons Right to Know</a></li>
+							<li class="pdf"><a href="https://sunset.provo.edu/wp-content/uploads/2022/01/2022-SSV-School-Improvement-Plan.pdf">2021-2022 School Improvement Plan</a></li>
+							<li class="pdf"><a href="https://sunset.provo.edu/wp-content/uploads/2022/01/2021-2022-Annual-Title-I-Meeting.pdf">2021-2022 Annual Title I Meeting</a></li>
+							<li class="pdf"><a href="https://sunset.provo.edu/wp-content/uploads/2022/01/2022-SSV-School-Needs-Assessment.pdf">2021-2022 School Needs Assessment</a></li>
+						</ul>
+						<p>Title I Coordinator: Lance Vandermark (LanceV@provo.edu)</p>
+				</div>
+				<div class="dropDownColumn">
+				<?php
+					//checks if this template section is loading into the menu or into the sidebar/content area and assigns the proper heading
+					if(is_page( 'title-i' ) || in_array( 623, get_post_ancestors($post))){
+						?>
+						<h2><img src="https://sunset.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/procedures1.svg" alt="" />School-Parent Compact</h2>
+						<?php
+					} else {
+						?>
+						<h3><img src="https://sunset.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/procedures1.svg" alt="" />School-Parent Compact</h3>
+						<?php
+					}
+				?>
+						<ul>
+							<li class="pdf"><a href="https://sunset.provo.edu/wp-content/uploads/2022/10/School-Parent-Family-Compact.pdf">School-Parent Compact</a></li>
+							<li class="pdf"><a lang="es" href="https://sunset.provo.edu/wp-content/uploads/2022/10/Pacto-Escuela-Padres-Familia.pdf">Pacto Entre Escuela y Padres</a></li>
+						</ul>
+				<?php
+				//checks if this template section is loading into the menu or into the sidebar/content area and assigns the proper heading
+				if(is_page( 'title-i' ) || in_array( 623, get_post_ancestors($post))){
+					?>
+					<h2><img src="https://sunset.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/procedures1.svg" alt="" />Parent Engagement Policy</h2>
+					<?php
+				} else {
+					?>
+					<h3><img src="https://sunset.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/procedures1.svg" alt="" />Parent Engagement Policy</h3>
+					<?php
+				}
+			?>
+						<ul>
+							<li class="pdf"><a href="https://sunset.provo.edu/wp-content/uploads/2022/10/Parent-Family-Engagement-Policy-2022.pdf">Parent and Family Engagement Policy</a></li>
+							<li class="pdf"><a lang="es" href="https://sunset.provo.edu/wp-content/uploads/2022/10/Politica-participacion-padres-familias-2022.pdf">Política de participación de los padres y las familias</a></li>
+						</ul>
+				</div>
+				<div class="dropDownColumn">
+					<?php
+						//checks if this template section is loading into the menu or into the sidebar/content area and assigns the proper heading
+						if(is_page( 'title-i' ) || in_array( 623, get_post_ancestors($post))){
+							?>
+							<h2><img src="https://amelia.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Additional Resources</h2>
+							<?php
+						} else {
+							?>
+							<h3><img src="https://amelia.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Additional Resources</h3>
+							<?php
+						}
+					?>
+						<ul>
+							<li class="int"><a href="https://provo.edu/preschool/title-i-preschool-registration/">Preschool Screener Registration</a></li>
+							<li class="pdf"><a href="https://sunset.provo.edu/wp-content/uploads/2021/12/public-news-story-2020-2021-schoolreport-card.pdf">School Report Card</a></li>
+							<li class="ext"><a href="https://provo.edu/title-i/">District Title I</a></li>
+						</ul>
+				</div>
