80caa69ca39e5361835a7fb06fccf0a82dfc40fe
Espi <joshespi@gmail.com>
2023-01-09 15:17:14 -0700

    wo100122

--- pcsd-2019-theme/functions.php
+++ pcsd-2019-theme/functions.php
@@ -8,7 +8,7 @@ function theme_specific_stylesheet()
     wp_register_script('404easterEgg', get_template_directory_uri() . '/assets/js/404.js', '', '', true);
 
     /*   CALL ALL CSS AND SCRIPTS FOR SITE */
-    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css', '', '1.0.07', false);
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css', '', '1.0.11', false);
     wp_enqueue_script('slick-script', get_template_directory_uri() . '/assets/slick/slick.min.js', array('jquery'), null, true);
     wp_enqueue_script('my-custom-scripts', get_template_directory_uri() . '/assets/js/scripts.js', array('jquery', 'slick-script',), '1.0.02', true);
     if (is_404()) {
