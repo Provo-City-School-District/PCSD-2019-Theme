40fc3fe583bea2d6e8ebc6a7d52eeb06406d79c0
Espi <joshespi@gmail.com>
2023-05-08 15:20:30 -0600

    WO103849->comment#273961

--- edgemont-child/mainmenu.php
+++ edgemont-child/mainmenu.php
@@ -7,19 +7,31 @@
 		<li class="droppable"><a href="<?php echo get_home_url(); ?>/school-information/">School Information</a>
 			<div class="mega-menu">
 				<h2>School Information</h2>
-				 <?php get_template_part( 'template-parts/mega-menu-dropdowns', 'schoolInfo'); ?>
+				<?php
+			// get_template_part('template-parts/mega-menu-dropdowns', 'schoolInfo'); 
+			$schoolinfo_menu = get_post(117);
+			echo do_shortcode($schoolinfo_menu->post_content);
+			?>
 			</div>
 		</li>
 		<li class="droppable"><a href="<?php echo get_home_url(); ?>/policies-forms/" aria-haspopup="true">Policies &amp; Forms</a>
 			<div class="mega-menu" aria-label="submenu">
 				<h2>Policies &amp; Forms</h2>
-					<?php get_template_part( 'template-parts/mega-menu-dropdowns', 'policies'); ?>
+				<?php
+			// get_template_part('template-parts/mega-menu-dropdowns', 'policies'); 
+			$policy_menu = get_post(122);
+			echo $policy_menu->post_content;
+			?>
 			</div>
 		</li>
 		<li class="droppable"><a href="<?php echo get_home_url(); ?>/faculty-staff/" aria-haspopup="true">Teachers &amp; Staff</a>
 			<div class="mega-menu" aria-label="submenu">
 				<h2>Teachers &amp; Staff</h2>
-					<?php get_template_part( 'template-parts/mega-menu-dropdowns', 'staff'); ?>
+				<?php
+			//get_template_part( 'template-parts/mega-menu-dropdowns', 'staff'); 
+			$faculty_Menu = get_post(70);
+			echo $faculty_Menu->post_content;
+			?>
 			</div>
 		</li>
 		
