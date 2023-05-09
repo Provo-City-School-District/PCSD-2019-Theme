150e1abb1ce903f760c77c8aa58b519b0ab67115
Espi <joshespi@gmail.com>
2023-05-09 09:26:05 -0600

    WO103849 -> com274020

--- franklin-child/sidebar-about.php
+++ franklin-child/sidebar-about.php
@@ -1,4 +1,7 @@
 <aside id="mainSidebar">
 	<h1>School Information</h1>
-	<?php echo do_shortcode( '[schoolinfo]' ); ?>
+	<?php
+	$schoolinfo_menu = get_post(117);
+	echo do_shortcode($schoolinfo_menu->post_content);
+	?>
 </aside>
