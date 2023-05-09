4e0c4efcac2771e89e8945a09edb4e6831cf33c3
Espi <joshespi@gmail.com>
2023-05-09 09:25:44 -0600

    Update sidebar-title-i.php

--- amelia-child/sidebar-title-i.php
+++ amelia-child/sidebar-title-i.php
@@ -1,8 +1,7 @@
 <aside id="mainSidebar">
 	<h1>Title I</h1>
-	<?php //echo do_shortcode( '[titleiMenu]' ); ?>
-	<?php 
-		$titleI_menu = get_post(623); 
-		echo $titleI_menu->post_content;
+	<?php
+	$titleI_menu = get_post(623);
+	echo $titleI_menu->post_content;
 	?>
 </aside>
