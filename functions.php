3c1c6bf80c52a17c681083234bc6acb1a9fd2322
Josh Espinoza <joshespi@gmail.com>
2025-01-24 12:18:52 -0700

    Implement collapsible content feature and update asset version to 1.0.6

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
-$asset_version = '1.0.5-1';
+$asset_version = '1.0.6';
 /*==========================================================================================
 Add stylesheet to enqueue
 ============================================================================================*/
@@ -1155,3 +1155,34 @@ function my_plugin_unregister_my_patterns()
 }
 add_action('init', 'my_plugin_unregister_my_patterns');
 //add_filter( 'show_admin_bar', '__return_true' );
+
+function collapsible_area_shortcode($atts, $content = null)
+{
+    static $collapsible_area_counter = 0;
+    $collapsible_area_counter++;
+
+    $atts = shortcode_atts(
+        array(
+            'title' => 'Click to Expand',
+            'heading' => 'h2', // Default heading level
+        ),
+        $atts,
+        'collapsible_area'
+    );
+
+    $heading_tag = in_array($atts['heading'], array('h2', 'h3')) ? $atts['heading'] : 'h2';
+    $unique_id = 'collapsible-area-' . $collapsible_area_counter;
+
+    ob_start();
+?>
+    <div class="collapsible-area" id="<?php echo $unique_id; ?>">
+        <<?php echo $heading_tag; ?> class="collapsible-button"><?php echo esc_html($atts['title']); ?></<?php echo $heading_tag; ?>>
+        <div class="collapsible-content" style="display: none;">
+            <?php echo do_shortcode($content); ?>
+        </div>
+    </div>
+
+<?php
+    return ob_get_clean();
+}
+add_shortcode('collapsible_area', 'collapsible_area_shortcode');
